b27ef339821254032b7fd19246dff477016957b5
enkeli <151573055+enkelii6@users.noreply.github.com>
2024-11-09 01:20:10 +0500

    Initialize main

---
+++ code/config.py
@@ -0,0 +1,20 @@
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    database_url: PostgresDsn
+
+
+settings = Settings()
+
+
+TORTOISE_CONFIG = {
+    'connections': {'default': str(settings.database_url)},
+    'apps': {
+        'models': {
+            'models': ['code.models', 'aerich.models'],
+            'default_connection': 'default',
+        },
+    },
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# FastAPI Tortoise template
+
+## You should have installed pre-commit and ruff locally:
+```
+# pip install ruff
+# pip install pre-commit
+# pre-commit install
+```
+## This will activate ```ruff check --fix``` anytime you want to commit
+
+### TODO: .dockerignore, github actions ci/cd, helm values
+
+### P.S. feel free to open PR with changes
